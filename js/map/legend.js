3e7ea5a12513fdb6d93a1409827067f0cd0a2697
Philipp Bous <pb@leibniz-psychology.org>
2025-10-07 21:48:11 +0200

    TRANSLATES legend to german

--- js/map/legend.js
+++ js/map/legend.js
@@ -11,46 +11,46 @@ export class Legend extends L.Control {
         this._map = map;
         const container = L.DomUtil.create('div', 'leaflet-control legendControl collapsed');
         container.innerHTML = `
-            <button class="legendToggle" title="show/hide legend and filters" type="button">☰ Legend and Filters</button>
+            <button class="legendToggle" title="show/hide legend and filters" type="button">☰ Legende und Filter</button>
             <div class="legendContent">
                 <fieldset>
-                    <legend>Legend and Filters</legend>
+                    <legend>Legende und Filter</legend>
                     <div class="legend-items">
                         <span class="legendLine">
                             <input type="checkbox" class="legendFilter" id="legend-place" name="legend-type" value="place" checked>
-                            <label for="legend-place" class="legendItem_label"><i class="icon-medium hgi hgi-stroke hgi-image-02"></i> Place</label>
+                            <label for="legend-place" class="legendItem_label"><i class="icon-medium hgi hgi-stroke hgi-image-02"></i> Ort</label>
                         </span>
                         <span class="legendLine">
                             <input type="checkbox" class="legendFilter" id="legend-architecture" name="legend-type" value="architecture" checked>
-                            <label for="legend-architecture" class="legendItem_label"><i class="icon-medium hgi hgi-stroke hgi-guest-house"></i> Architecture</label>
+                            <label for="legend-architecture" class="legendItem_label"><i class="icon-medium hgi hgi-stroke hgi-guest-house"></i> Architektur</label>
                         </span>
                         <span class="legendLine">
                             <input type="checkbox" class="legendFilter" id="legend-object" name="legend-type" value="object" checked>
-                            <label for="legend-object" class="legendItem_label"><i class="icon-medium hgi hgi-stroke hgi-sword-02"></i> Object</label>
+                            <label for="legend-object" class="legendItem_label"><i class="icon-medium hgi hgi-stroke hgi-sword-02"></i> Gegenstand</label>
                         </span>
                         <span class="legendLine">
                             <input type="checkbox" class="legendFilter" id="legend-document" name="legend-type" value="document" checked>
-                            <label for="legend-document" class="legendItem_label"><i class="icon-medium hgi hgi-stroke hgi-graduation-scroll"></i> Document</label>
+                            <label for="legend-document" class="legendItem_label"><i class="icon-medium hgi hgi-stroke hgi-graduation-scroll"></i> Dokument</label>
                         </span>
                         <span class="legendLine">
                             <input type="checkbox" class="legendFilter" id="legend-event" name="legend-type" value="event" checked>
-                            <label for="legend-event" class="legendItem_label"><i class="icon-medium hgi hgi-stroke hgi-calendar-01"></i> Event</label>
+                            <label for="legend-event" class="legendItem_label"><i class="icon-medium hgi hgi-stroke hgi-calendar-01"></i> Ereignis</label>
                         </span>
                         <span class="legendLine">
                             <input type="checkbox" class="legendFilter" id="legend-immaterial" name="legend-type" value="immaterial" checked>
-                            <label for="legend-immaterial" class="legendItem_label"><i class="icon-medium hgi hgi-stroke hgi-border-none-02"></i> Immaterial</label>
+                            <label for="legend-immaterial" class="legendItem_label"><i class="icon-medium hgi hgi-stroke hgi-border-none-02"></i> immateriell</label>
                         </span>
                         <span class="legendLine">
                             <input type="checkbox" class="legendFilter" id="legend-other" name="legend-type" value="other" checked>
-                            <label for="legend-other" class="legendItem_label"><i class="icon-medium hgi hgi-stroke hgi-flag-01"></i> Other</label>
+                            <label for="legend-other" class="legendItem_label"><i class="icon-medium hgi hgi-stroke hgi-flag-01"></i> Sonstiges</label>
                         </span>
                         <span class="legendLine">
                             <input type="checkbox" class="legendFilter" id="legend-unlabeled" name="legend-type" value="unlabeled" checked>
-                            <label for="legend-unlabeled" class="legendItem_label"><i class="icon-medium hgi hgi-stroke hgi-help-circle"></i> Unlabeled</label>
+                            <label for="legend-unlabeled" class="legendItem_label"><i class="icon-medium hgi hgi-stroke hgi-help-circle"></i> ohne Angabe</label>
                         </span>
                     </div>
                 </fieldset>
-                <button type="button" class="legendEnableAllButton" title="Reset filters/show all">Reset filters</button>
+                <button type="button" class="legendEnableAllButton" title="Reset filters/show all">Filter zurücksetzen</button>
             </div>
         `;
         // Prevent map interaction when clicking legend
